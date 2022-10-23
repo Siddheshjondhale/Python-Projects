5603f98ff1ebd705d3f5770b39148ef4305e9695
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2022-10-24 04:59:57 +0530

    code doesnt worl so I comit sucide ;(

--- desktop_notification.py
+++ desktop_notification.py
@@ -5,18 +5,29 @@ tkin.title("Notification App") #Title of the applicaiton
 tkin.geometry("300x300") #Size of the application
 
 #labels
-label1 = Label(tkin, text='Title of the notification').grid(row=0,column=0,)
+titleLabel = Label(tkin, text='Title of the notification').grid(row=0,column=0,)
 label2 = Label(tkin, text='Message of the notification').grid(row=1,column=0)
 label3 = Label(tkin, text='After how much time notification should display').grid(row=2,column=0)
+
+#function
+def data():
+    #title = titleLabel.get()
+    print(e2.get())
 
 #entry
 e1 = Entry(tkin).grid(row = 0, column=1)
 e2 = Entry(tkin).grid(row = 1, column=1)
 e3 = Entry(tkin).grid(row = 2, column=1)
 
-#button
 
-button = Button(tkin, text='Stop', width=25,).place(x=0,y=85)
+
+
+#button
+button = Button(tkin, text='Stop', width=25,command=data).place(x=0,y=85)
+
+
+
+
 
 
 tkin.mainloop() #this will help to run the application in a loop
@@ -27,6 +38,5 @@ tkin.mainloop() #this will help to run the application in a loop
 
 
 #this gives desktop notification
-nd.notify(title='Test', message='Test 123', app_name='12', app_icon='', timeout=10, toast=False,)
+#nd.notify(label1, message='Test 123', app_name='12', app_icon='', timeout=10, toast=False,)
 
-print("gel")
