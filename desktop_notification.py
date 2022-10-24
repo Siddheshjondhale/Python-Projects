eb63b6d68b126fe238d443d9c274c78d3c9558a8
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2022-10-24 09:25:24 +0530

    Fixed Bugs and Completed Desktop Notification App

--- desktop_notification.py
+++ desktop_notification.py
@@ -1,42 +1,64 @@
 from plyer import notification as nd
 from tkinter import *
+import pyautogui
 tkin = Tk()
 tkin.title("Notification App") #Title of the applicaiton
-tkin.geometry("300x300") #Size of the application
+tkin.geometry("500x500") #Size of the application
 
 #labels
-titleLabel = Label(tkin, text='Title of the notification').grid(row=0,column=0,)
-label2 = Label(tkin, text='Message of the notification').grid(row=1,column=0)
-label3 = Label(tkin, text='After how much time notification should display').grid(row=2,column=0)
+titleLabel = Label(tkin, text='Title of the notification')
+titleLabel.grid(row=0,column=0)
+messageLabel = Label(tkin, text='Message of the notification')
+messageLabel.grid(row=1,column=0)
+time = Label(tkin, text='After how much time notification should display')
+time.grid(row=2,column=0)
 
 
 
-#entry
-e1 = Entry(tkin).grid(row = 0, column=1)
-e2 = Entry(tkin).grid(row = 1, column=1)
-e3 = Entry(tkin).grid(row = 2, column=1)
+#entryz
+titleEntry = Entry(tkin)
+titleEntry.grid(row = 0, column=1)
+messageEntry = Entry(tkin)
+messageEntry.grid(row = 1, column=1)![](../../../../AppData/Local/Temp/154495987-d3c05b34-161d-427d-8543-eaa5d6ff4516.png)
+timeEntry = Entry(tkin)
+timeEntry.grid(row = 2, column=1)
+# e4 = Entry(Tk()).grid(row = 3, column=1)
 
 #function
 def data():
-    #testing
+    try:
+        title = titleEntry.get()
+        message = messageEntry.get()
+        Timeout = int(float((timeEntry.get()))) #because we cant put time in decimals it takes only integer value
+
+    except:
+        # validation
+        if title == "" or message == "" or Timeout == "":
+            pyautogui.alert(text='You need to enter all the values', title='Error', button='OK')
+            print("THis ran 1")
+
+    nd.notify(title=title, message=message, app_name='notifier', timeout=Timeout, ticker="notification App",toast=True)
+
 
 
 
 #button
-button = Button(tkin, text='Stop', width=25,command=data).place(x=0,y=85)
+button = Button(tkin, text='Submit', width=25,command=data).place(x=0,y=85)
 
 
 
 
 
 
-tkin.mainloop() #this will help to run the application in a loop
-# tkin.resizable() #this function helps to resize the application
+tkin.mainloop()
+#this will help to run the application in a loop
+tkin.resizable()
+#this function helps to resize the application
 
 
 
 
 
 #this gives desktop notification
-#nd.notify(label1, message='Test 123', app_name='12', app_icon='', timeout=10, toast=False,)
+nd.notify(title="Test", message='Test 123', app_name='12', app_icon='', timeout=10, toast=False,)
 
